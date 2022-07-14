6abfd4e53abe1a22dd5bf88cd757e51d9a09c5c8
shefali <shefali.sethiya@placiddigital.com>
2022-07-14 18:26:09 +0530

    implent template engin

--- stisla-admin-template-master/leaveschema.js
+++ stisla-admin-template-master/leaveschema.js
@@ -4,7 +4,7 @@ const empSchema = new mongoose.Schema({
     leavetype: { type: String },
     from: { type: String },
     to: { type: String },
-    comment: { type: Number },
+    comment: { type: String},
 
 })
 module.exports = mongoose.model('leave', empSchema)
